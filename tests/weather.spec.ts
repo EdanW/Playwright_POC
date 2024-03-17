2bd1c558bb48632f0a812bba0051f32f025bacdd
Edan Weinberg <edanweinberg@mail.tau.ac.il>
2024-03-17 11:03:25 +0200

    changed weather test

--- tests/weather.spec.ts
+++ tests/weather.spec.ts
@@ -1,7 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('is it 18 degrees somewhere', async ({ page }) => {
+// This test checks whether at some city it is 18 degrees.
+// It uses some JS, ElementHandle and $$ function
+test('it is 18 degrees somewhere by ElementHandle', async ({ page }) => {
   await page.goto('https://www.accuweather.com//');
+  
+  const cities = await page.$$('[class="text temp"]');
+  var isEighteenSomewhere = false;
+  for (const city of cities) {
+    const currTemp = await city.innerText();
+    if (currTemp == "18°"){
+      isEighteenSomewhere = true;
+      break;
+    }
+  }
 
-  await expect(page.getByText('18°').first()).toBeVisible();
+  expect(isEighteenSomewhere).toBeTruthy();
+  await page.close();
 });
