1e6ceca50680bc2bb454411d4155f6a22376d953
Edan Weinberg <edanweinberg@mail.tau.ac.il>
2024-03-17 18:31:18 +0200

    farenheit

--- tests/weather.spec.ts
+++ tests/weather.spec.ts
@@ -2,16 +2,15 @@ import { test, expect } from '@playwright/test';
 
 // This test checks whether at some city it is 18 degrees.
 // It uses some JS, ElementHandle and $$ function
-test('it is 18 degrees somewhere by ElementHandle', async ({ page }) => {
+test('it is 60 degrees somewhere by ElementHandle', async ({ page }) => {
   await page.goto('https://www.accuweather.com//');
   
   const cities = await page.$$('[class="text temp"]');
   var isEighteenSomewhere = false;
   for (const city of cities) {
-    console.log (city);
     const currTemp = await city.innerText();
     console.log (currTemp);
-    if (currTemp == "18°"){
+    if (currTemp == "60°"){
       isEighteenSomewhere = true;
       break;
     }
